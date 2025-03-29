ef082a50b99b6ebb2c5613e107c4e102d764af23
GelaPostrero <angelapostrero@gmail.com>
2025-03-29 21:25:34 +0800

    Add user creation functionality

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,22 @@
-import { Router } from "express"
-export const userRouter = Router()
+import { Router, Request, Response } from "express";
+import { AppDataSource } from "../data-source";
+import { User } from "../entities/Users";
+
+const router = Router();
+const userRepository = AppDataSource.getRepository(User);
+
+// CREATE ONE USER
+//userRouter.post('/', createUser);
+router.post('/', async (req: Request, res: Response) => {
+    try {
+        const { firstname, lastname, middlename, email, password } = req.body;
+        const user = userRepository.create({ firstname, lastname, middlename, email, password });
+        await userRepository.save(user);
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).json({ message: "Error creating user", error });
+    }
+});
 
 //DELETE ONE USER
 //userRouter.delete('/:id', deleteUser)
@@ -10,5 +27,3 @@ export const userRouter = Router()
 // GET ONE USER BY ID
 //userRouter.get("/:id", getUserById);
 
-// CREATE ONE USER
-//userRouter.post('/', createUser);
